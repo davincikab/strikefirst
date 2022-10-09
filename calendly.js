dcabeb986fe108bc6a8cd120e8b41ed773200174
highiqdev <115380494+highiqdev@users.noreply.github.com>
2022-10-08 18:42:44 -0600

    Add files via upload
    original upload

---
+++ calendly.js
@@ -0,0 +1,5 @@
+// <!-- Calendly link widget begin -->
+// <link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet">
+// <script src="https://assets.calendly.com/assets/external/widget.js" type="text/javascript" async></script>
+// <a href="" onclick="Calendly.initPopupWidget({url: 'https://calendly.com/highiq-marketing/strike-first-lead-campaign'});return false;"></a>
+// <!-- Calendly link widget end -->
